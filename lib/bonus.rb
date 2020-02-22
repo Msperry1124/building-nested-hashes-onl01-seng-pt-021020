8feb3ef8a6e82596d6671fe2c792e31d6d99b4d1
Melissa Sperry <sperry.mel@gmail.com>
2020-02-22 05:50:24 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,6 +1,6 @@
-def bonus
-  epic_tragedy[:montague][:hero,:status] = "dead"
-  epic_tragedy[:capulet][:heroine,:status] = "dead"
+def bonus 
+  do epic_tragedy[:montague][:hero,:status] = "dead"
+  do epic_tragedy[:capulet][:heroine,:status] = "dead"
   puts epic_tragedy
   epic_tragedy = {
    :montague => {
